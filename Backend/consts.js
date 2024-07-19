499bbf13d26a14234fb6ce4bed176acfbac0e3a5
German Salas Martinez <germansm@meta.com>
2024-07-19 11:38:44 -0700

    backend meals.js add(route to get the average like of the user to all cuisines)

--- Backend/consts.js
+++ Backend/consts.js
@@ -4,6 +4,7 @@ exports.USER_ROUTE = '/users';
 exports.USER_GOAL_ROUTE = '/goal';
 
 exports.MEAL_ROUTE = '/meals';
+exports.MEAL_CUISINE_ROUTE = '/cuisine'
 
 exports.HISTORY_ROUTE = '/history';
 exports.HISTORY_REGISTERED_DAYS_ROUTE = '/registered_days';
--- Backend/routes/meals.js
+++ Backend/routes/meals.js
@@ -1,5 +1,6 @@
 const express = require('express'), router = express.Router();
 const { PrismaClient } = require('@prisma/client');
+const { MEAL_CUISINE_ROUTE } = require('../consts');
 const prisma = new PrismaClient();
 
 router.use('/', (req, res, next) => {
@@ -17,6 +18,58 @@ router.get('/:id', async (req, res) => {
     res.json(meal);
 })
 
+// Getting an average of how much the user likes a cuisine by getting all the meals from the user and
+// put in a map the cuisine as and the like or dislike in an array as value. then go through the map
+// to get the weighted average of each cuisine array and put that into a json format string
+router.get('/cuisine' + '/:id', async (req, res) => {
+    const userId = req.params.id;
+    const cuisines = await prisma.meals.findMany({
+        where: {
+            userId
+        },
+        select: {
+            cuisine: true,
+            like: true
+        }
+    })
+
+    const mapCuisines = new Map()
+
+    for (let cuisine of cuisines) {
+        if (!mapCuisines.has(cuisine.cuisine)) {
+            if (cuisine.like === true) {
+                mapCuisines.set(cuisine.cuisine, [1])
+            } else {
+                mapCuisines.set(cuisine.cuisine, [0])
+            }
+        } else {
+            if (cuisine.like === true) {
+                mapCuisines.get(cuisine.cuisine).push(1)
+            } else {
+                mapCuisines.get(cuisine.cuisine).push(0)
+            }
+        }
+    }
+
+    var jsonStr = '[{'
+    for (let cuisine of mapCuisines) {
+        let likeCount = 0
+        for (let likes of cuisine[1]) {
+            if (likes) {
+                likeCount++
+            }
+        }
+        let weightAverage = likeCount / cuisine[1].length
+        jsonStr += `"${cuisine[0]}": ${weightAverage},`
+    }
+
+    jsonStr = jsonStr.slice(0, -1)
+    jsonStr += '}]'
+    const jsonRes = JSON.parse(jsonStr)
+
+    res.json(jsonRes)
+})
+
 // Posting a meal following the next types
 /*
     name: string;
@@ -26,11 +79,13 @@ router.get('/:id', async (req, res) => {
     carbs: number;
     fats: number;
     calories: number;
+    cuisine: string;
+    like: boolean;
     date: string;
     userId: string;
 */
 router.post('/', async (req, res) => {
-    const { name, category, description, proteins, carbs, fats, calories, date, userId } = req.body;
+    const { name, category, description, proteins, carbs, fats, calories, cuisine, like, date, userId } = req.body;
     const meal = await prisma.meals.create({
         data: {
             name,
@@ -40,6 +95,8 @@ router.post('/', async (req, res) => {
             carbs,
             fats,
             calories,
+            cuisine,
+            like,
             date,
             userId
         }
