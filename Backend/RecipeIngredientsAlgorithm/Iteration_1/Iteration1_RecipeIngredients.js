9ac89f0e724f023ddada1f47514361ba717d351b
German Salas Martinez <germansm@meta.com>
2024-07-22 14:05:39 -0700

    Fourth iteration recipe recommendation / added ideal kilocalories based on user goal and already eaten / improving readability by moving code to get points into functions / changed comparison of ingredients

--- Backend/RecipeIngredientsAlgorithm/Iteration_1/Iteration1_RecipeIngredients.js
+++ Backend/RecipeIngredientsAlgorithm/Iteration_1/Iteration1_RecipeIngredients.js
@@ -5,6 +5,7 @@ const data = [
         "protein": 25,
         "carbs": 40,
         "fats": 20,
+        "calories": 1900,
         "cuisine": ["italian"],
         "mealType": "lunch",
         "totalTime": 25.0
@@ -15,6 +16,7 @@ const data = [
         "protein": 30,
         "carbs": 35,
         "fats": 25,
+        "calories": 1600,
         "cuisine": ["american"],
         "mealType": "dinner",
         "totalTime": 15.0
@@ -25,6 +27,7 @@ const data = [
         "protein": 20,
         "carbs": 30,
         "fats": 15,
+        "calories": 1800,
         "cuisine": ["mexican"],
         "mealType": "breakfast",
         "totalTime": 10.0
@@ -35,6 +38,7 @@ const data = [
         "protein": 10,
         "carbs": 20,
         "fats": 10,
+        "calories": 200,
         "cuisine": ["american"],
         "mealType": "lunch",
         "totalTime": 5.0
@@ -45,6 +49,7 @@ const data = [
         "protein": 35,
         "carbs": 30,
         "fats": 20,
+        "calories": 1400,
         "cuisine": ["american"],
         "mealType": "lunch",
         "totalTime": 10.0
@@ -55,6 +60,7 @@ const data = [
         "protein": 25,
         "carbs": 30,
         "fats": 15,
+        "calories": 2800,
         "cuisine": ["japanese"],
         "mealType": "dinner",
         "totalTime": 20.0
@@ -65,6 +71,7 @@ const data = [
         "protein": 20,
         "carbs": 40,
         "fats": 20,
+        "calories": 2000,
         "cuisine": ["chinese"],
         "mealType": "dinner",
         "totalTime": 15.0
@@ -75,6 +82,7 @@ const data = [
         "protein": 30,
         "carbs": 30,
         "fats": 20,
+        "calories": 1800,
         "cuisine": ["indian"],
         "mealType": "dinner",
         "totalTime": 20.0
@@ -85,6 +93,7 @@ const data = [
         "protein": 25,
         "carbs": 40,
         "fats": 20,
+        "calories": 1400,
         "cuisine": ["italian"],
         "mealType": "dinner",
         "totalTime": 25.0
@@ -95,6 +104,7 @@ const data = [
         "protein": 20,
         "carbs": 30,
         "fats": 15,
+        "calories": 500,
         "cuisine": ["chinese"],
         "mealType": "dinner",
         "totalTime": 10.0
@@ -105,6 +115,7 @@ const data = [
         "protein": 15,
         "carbs": 30,
         "fats": 10,
+        "calories": 700,
         "cuisine": ["american"],
         "mealType": "lunch",
         "totalTime": 20.0
@@ -115,6 +126,7 @@ const data = [
         "protein": 15,
         "carbs": 35,
         "fats": 20,
+        "calories": 1300,
         "cuisine": ["american"],
         "mealType": "lunch",
         "totalTime": 5.0
@@ -125,6 +137,7 @@ const data = [
         "protein": 25,
         "carbs": 30,
         "fats": 20,
+        "calories": 1500,
         "cuisine": ["mexican"],
         "mealType": "dinner",
         "totalTime": 15.0
@@ -135,6 +148,7 @@ const data = [
         "protein": 30,
         "carbs": 20,
         "fats": 15,
+        "calories": 1000,
         "cuisine": ["american"],
         "mealType": "dinner",
         "totalTime": 10.0
@@ -145,90 +159,14 @@ const data = [
         "protein": 5,
         "carbs": 40,
         "fats": 20,
+        "calories": 800,
         "cuisine": ["american"],
         "mealType": "lunch",
         "totalTime": 45.0
     }
 ]
 
-// Constants of user inputs
-const USER_INGREDIENTS = ["potato", "butter", "cheese"]
-const USER_TIME_COOK = 'fast'
-const USER_RANDOMNESS = 100 / 100
-
-// Constants of points distribution
-const OVERLAP_POINTS = 10 * (1 - USER_RANDOMNESS)
-const MEAL_TYPE_POINTS = 3
-const TIME_COOK_POINTS = 8
-const RANDOM_POINTS = 10
-
-const TOP_1_2_CUISINE_POINTS = 3
-const TOP_3_4_CUISINE_POINTS = 2
-const TOP_5_CUISINE_POINTS = 1
-
-const maxRandomPoints = RANDOM_POINTS * USER_RANDOMNESS
-
-
-// Function to get overlapping elements between two arrays,
-// by adding their lengths and substract by the size of a set of the two
-// arrays combined, so that in the set are only distinct values and if this
-// value is less than the two arrays combined it means there is duplicates
-const overlapIngredients = (foodIngredients, userIngredients) => {
-    const numberOverlapping = foodIngredients.length + userIngredients.length - new Set(foodIngredients.concat(userIngredients)).size
-    const similarPercent = numberOverlapping / userIngredients.length
-    return (OVERLAP_POINTS * similarPercent)
-}
-
-// Helper function to sort the foods by score
-// food = [mealObject, score]
-const compareFoods = (a, b) => {
-    return b[1] - a[1]
-}
-
-// Helper function to sort cuisine by score
-// {"cuisine": score}
-const compareCuisine = (a, b) => {
-    return b[Object.keys(b)[0]] - a[Object.keys(a)[0]]
-}
-
-// Function to generate a random number between a minimum and a maximum,
-// the random number is rounded to two decimal points
-function getRandomNumber(min, max) {
-    const randomValue = Math.random() * (max - min) + min;
-    const roundTwoDecimalsRandom = Math.round(randomValue * 100) / 100
-    return roundTwoDecimalsRandom
-}
-
-// Function to get the total score of the meal
-const getRankMeal = (food, overlap, cuisine, hour, USER_TIME_COOK) => {
-    var rank = overlap + cuisine
-
-    if (5 <= hour && hour < 12) {
-        if (food.mealType === 'breakfast') {
-            rank += MEAL_TYPE_POINTS
-        }
-    } else if (12 <= hour && hour < 6) {
-        if (food.mealType === 'lunch' || food.mealType === 'lunch/dinner') {
-            rank += MEAL_TYPE_POINTS
-        }
-    } else if (6 <= hour && hour <= 23) {
-        if (food.mealType === 'dinner' || food.mealType === 'lunch/dinner') {
-            rank += MEAL_TYPE_POINTS
-        }
-    }
-
-    if (USER_TIME_COOK === 'fast' && food.totalTime <= 25) {
-        rank += TIME_COOK_POINTS
-    } else if (USER_TIME_COOK === 'normal' && (25 < food.totalTime && food.totalTime <= 60)) {
-        rank += TIME_COOK_POINTS
-    } else if (USER_TIME_COOK === 'long' && (60 < food.totalTime)) {
-        rank += TIME_COOK_POINTS
-    }
-
-    return rank
-}
-
-// Dummy data of cuisine scores
+// Dummy data of cuisine scores (got an API call to get this data)
 const cuisineType = [
     { "american": 0.64 },
     { "asian": 0.75 },
@@ -250,8 +188,145 @@ const cuisineType = [
     { "south east asian": 0.63 }
 ]
 
-cuisineType.sort(compareCuisine)
+// Constants of user inputs
+const USER_INGREDIENTS = ["potato", "butter", "cheese"]
+const USER_TIME_COOK = 'fast'
+const USER_RANDOMNESS = 0 / 100
 
+// Constants of user data
+const USER_KCAL_GOAL = 2500
+const USER_KCAL_TODAY = 1000
+
+// Constants of points distribution
+const OVERLAP_POINTS = 10 * (1 - USER_RANDOMNESS)
+const MEAL_TYPE_POINTS = 3
+const TIME_COOK_POINTS = 8
+const RANDOM_POINTS = 10
+const KCAL_POINTS = 8
+
+const TOP_1_2_CUISINE_POINTS = 3
+const TOP_3_4_CUISINE_POINTS = 2
+const TOP_5_CUISINE_POINTS = 1
+
+const maxRandomPoints = RANDOM_POINTS * USER_RANDOMNESS
+
+
+// Helper function to sort the foods by score
+// food = [mealObject, score]
+const compareFoods = (a, b) => {
+    return b[1] - a[1]
+}
+
+// Helper function to sort cuisine by score
+// {"cuisine": score}
+const compareCuisine = (a, b) => {
+    return b[Object.keys(b)[0]] - a[Object.keys(a)[0]]
+}
+
+// Function to generate a random number between a minimum and a maximum,
+// the random number is rounded to two decimal points
+function getRandomNumber(min, max) {
+    const randomValue = Math.random() * (max - min) + min;
+    const roundTwoDecimalsRandom = Math.round(randomValue * 100) / 100
+    return roundTwoDecimalsRandom
+}
+
+// Function to get the ideal calories for the user depending on the time
+// and the kcal goal that the user has, the percentages were decided by
+// looking up on the internet how to distribute the calories between meals
+// Breakfast = 30%        Lunch = 40%        Dinner = 40%
+// If the user skipped or had a really light breakfast then lunch gets an increase to 55%
+const getIdealKcal = (hour) => {
+    if (5 <= hour && hour < 12) {
+        return USER_KCAL_GOAL * 0.30
+    } else if (12 <= hour && hour < 6) {
+        if (USER_KCAL_TODAY < USER_KCAL_GOAL * 0.15) {
+            return USER_KCAL_GOAL * 0.55
+        } else {
+            return (USER_KCAL_GOAL - USER_KCAL_TODAY) * 0.40
+        }
+    } else if (6 <= hour && hour <= 23) {
+        return USER_KCAL_GOAL - USER_KCAL_TODAY
+    }
+
+    return 0
+}
+
+// Function to get overlapping elements between two arrays, by dividing the ingredient by spaces and then checking
+// and then adding their lengths and substract by the size of a set of the two arrays combined,
+// so that in the set are only distinct values and if this value is less than the two arrays combined it means
+// there are words that match, so we count it as overlap of ingredient
+const getOverlapIngredientsPoints = (foodIngredients, userIngredients) => {
+    let countOverlap = 0
+    for (let ingredientUser of userIngredients) {
+        const ingrU = ingredientUser.split(" ")
+        for (let ingredientFood of foodIngredients) {
+            const ingrF = ingredientFood.split(" ")
+            if ((ingrU.length + ingrF.length - new Set(ingrU.concat(ingrF)).size) > 0) {
+                countOverlap++
+            }
+        }
+    }
+    const similarPercent = countOverlap / userIngredients.length
+    return (OVERLAP_POINTS * similarPercent)
+}
+
+// Function to get the points of the user based on the hour and the type of meal
+const getHourMealTypePoints = (food, hour) => {
+    if (5 <= hour && hour < 12) {
+        if (food.mealType === 'breakfast') {
+            return MEAL_TYPE_POINTS
+        }
+    } else if (12 <= hour && hour < 6) {
+        if (food.mealType === 'lunch' || food.mealType === 'lunch/dinner') {
+            return MEAL_TYPE_POINTS
+        }
+    } else if (6 <= hour && hour <= 23) {
+        if (food.mealType === 'dinner' || food.mealType === 'lunch/dinner') {
+            return MEAL_TYPE_POINTS
+        }
+    }
+
+    return 0
+}
+
+// Function to see if the cooking time of a meal satisfies the user input
+const getTimeCookPoints = (food, USER_TIME_COOK) => {
+    if (USER_TIME_COOK === 'fast' && food.totalTime <= 25) {
+        return TIME_COOK_POINTS
+    } else if (USER_TIME_COOK === 'normal' && (25 < food.totalTime && food.totalTime <= 60)) {
+        return TIME_COOK_POINTS
+    } else if (USER_TIME_COOK === 'long' && (60 < food.totalTime)) {
+        return TIME_COOK_POINTS
+    }
+
+    return 0
+}
+
+// Function to check if a meal satisfy in a range of 10% the ideal calories for a meal
+const getIdealKcalPoints = (food, idealKcal) => {
+    if ((idealKcal * 0.9) < food.calories && food.calories < (idealKcal * 1.1)) {
+        return KCAL_POINTS
+    }
+
+    return 0
+}
+
+
+
+// Function to get the total score of the meal
+const getRankMeal = (food, cuisine, hour, idealKcal, USER_TIME_COOK, USER_INGREDIENTS) => {
+    let rank = cuisine
+
+    rank += getOverlapIngredientsPoints(food.ingredients, USER_INGREDIENTS);
+    rank += getHourMealTypePoints(food, hour)
+    rank += getTimeCookPoints(food, USER_TIME_COOK)
+    rank += getIdealKcalPoints(food, idealKcal)
+
+    return rank
+}
+
+cuisineType.sort(compareCuisine)
 const cuisineRanking = new Map()
 
 // Giving points to cuisines depending on the order the user has liked them
@@ -274,19 +349,15 @@ console.log('cuisine', cuisineRanking)
 // Getting the hour in a 24 hour format
 const date = new Date()
 const hour = Number(date.toLocaleTimeString("en-GB").slice(0, 2));
-
+const idealKcal = getIdealKcal(hour)
 
 const recommendationsArray = []
 
 // If randomness is not max then do a ranking depending on the user data
 if (0 <= USER_RANDOMNESS && USER_RANDOMNESS <= 0.99) {
     for (var food of data) {
-        const overlap = overlapIngredients(food.ingredients, USER_INGREDIENTS);
-
-        if (overlap > 0) {
-            if (cuisineRanking.has(food.cuisine[0])) {
-                recommendationsArray.push([food, getRankMeal(food, overlap, cuisineRanking.get(food.cuisine[0]), hour, USER_TIME_COOK)])
-            }
+        if (cuisineRanking.has(food.cuisine[0])) {
+            recommendationsArray.push([food, getRankMeal(food, cuisineRanking.get(food.cuisine[0]), hour, idealKcal, USER_TIME_COOK, USER_INGREDIENTS)])
         }
     }
 
@@ -295,6 +366,7 @@ if (0 <= USER_RANDOMNESS && USER_RANDOMNESS <= 0.99) {
             recommendation[1] += getRandomNumber(0, maxRandomPoints)
         }
     }
+
 } else {
     // If randomness is max then ignore all user data and just give random ranking
     for (var food of data) {
