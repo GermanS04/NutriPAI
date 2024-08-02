cf984ad1acc734b9c7aa7430907dd7a45aec91a9
German Salas Martinez <germansm@meta.com>
2024-08-02 10:51:46 -0700

    changes on loading state animation

--- Frontend/nutri-pal/src/components/loading/Loading.tsx
+++ Frontend/nutri-pal/src/components/loading/Loading.tsx
@@ -1,24 +1,12 @@
 'use client'
 import '@/styles/Loading.css'
-import { useEffect, useState } from 'react';
 
 export const Loading = () => {
-
-    const [dots, setDots] = useState(3);
-
-    // Use effect when rendering the component, increment the dots and do a remainder of 4 (cause it can only be 0 1 2 or 3) and do that every 500 milliseconds
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setDots((prevDots) => (prevDots + 1) % 4);
-        }, 250);
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <div className='loading-container'>
-            <div className='loading-overlay' />
             <div className='loading-text-container'>
-                <p className='loading-text'>Loading{Array(dots).fill('.').join('')}</p> {/* Make an array of X amount of dots and join them with empty string */}
+                <div className='loading-text-container-inner'>
+                </div>
             </div>
         </div>
     )
